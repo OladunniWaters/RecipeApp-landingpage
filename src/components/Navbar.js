0267edf31963401119f850feb6b2e342954e36dc
Oladunni Waters <waters.oladunni@gmail.com>
2023-01-04 12:18:36 -0800

    mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import { RxHamburgerMenu } from 'react-icons/rx';
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-        <nav class="navbar navbar-expand-lg bg-light">
+
+        <nav className="navbar sticky-top navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/navbar">Navbar</a>
+            <a class="navbar-brand" href="/navbar">Recipe App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+               <RxHamburgerMenu />
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                <a class="nav-link" aria-current="page" href="/home">Home</a>
                 </li>
+
                 <li class="nav-item">
-                <a class="nav-link" href="/link">Link</a>
+                <a class="nav-link" href="/about">About</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href='https://expo.dev/@oladunniwaters/RecipeApp?serviceType=classic&distribution=expo-go' target='_blank' rel="noreferrer"> Download</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="https://twitter.com/OladunniCath" target="_blank" rel="noreferrer">Contact</a>
                 </li>
             </ul>
             </div>
         </div>
         </nav>
-    </div>
+
   )
 }
 
